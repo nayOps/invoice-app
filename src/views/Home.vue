eb08c571018f0e5241338e3f65dab9d24db0a4fa
nayOps <raphaelalema@gmail.com>
2022-03-17 15:56:46 +0100

    Screen Responsive

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="home container">
+        <!-- header -->
+                
+        <div class="header">
+        <div class="left flex flex-column">
+            <h1>Invoices</h1>
+            <span>There are 4 total invoices</span>
+        </div>
+        <div class="right flex">
+            <div  @click="toggleFilterMenu" class="filter flex" ref="filter" >
+            </div>
+        </div>
+        </div>
+    </div>
+<template>
+
+<script>
+    export default{
+        name: "Home";
+        components:{
+        },
+    }
+</script>
